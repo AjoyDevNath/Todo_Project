304b771185babc5b4f1246e912c82c822badf058
Ajoy <ajoydev38@gmail.com>
2023-12-31 01:34:25 +0600

    creating todo

---
+++ index.js
@@ -0,0 +1,5 @@
+function cookMaggi(maggi,pani,tapeli){
+    console.log("Your maggie will be ready in "+ maggi * 2 +" minutes "+" and ingredients used are :- "+ maggi + " Maggi " +pani + " cup of water"+" using "+tapeli+" pan ");
+}
+
+cookMaggi(4,8,1);
